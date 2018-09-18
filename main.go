a30d9e84c2b17f14451f25e49aced95abde4696d
Sven <github@svenbuesing.de>
2018-09-18 23:52:08 +0200

    Add main.go

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"crypto/rand"
+	"flag"
+	"fmt"
+	"log"
+	"math/big"
+	"net"
+)
+
+func main() {
+	var service string
+	flag.StringVar(&service, "service", "", "servicename to resolve")
+	flag.Parse()
+
+	if service == "" {
+		fmt.Printf("usage:\n\n")
+		flag.PrintDefaults()
+		return
+	}
+
+	_, addrs, err := net.LookupSRV("", "", service)
+	if err != nil {
+		log.Fatalf("could not resolve service %q: %v", service, err)
+	}
+
+	if len(addrs) <= 0 {
+		log.Fatalf("no addresses found for %q", service)
+	}
+
+	r, err := rand.Int(rand.Reader, big.NewInt(int64(len(addrs))))
+	if err != nil {
+		log.Fatalf("could not generate randomness: %v", err)
+	}
+
+	// Print random srv name
+	fmt.Print(addrs[r.Int64()].Target)
+}
